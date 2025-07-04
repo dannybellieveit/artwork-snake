e0d07b0eaf011e4f5cce966c9a1dc62fa4ea92a1
dannybellieveit <131538258+dannybellieveit@users.noreply.github.com>
2025-07-04 22:10:56 +0100

    Add rewrite for list-proxy and revert drop page fetch

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ module.exports = {
         source: '/drop/:token',    // any /drop/XYZ
         destination: '/drop/index.html', // serve the static wrapper
       },
+      {
+        source: '/list-proxy/:token',
+        destination: '/api/list-proxy/:token',
+      },
     ];
   },
 };
