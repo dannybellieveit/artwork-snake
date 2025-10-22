c4e25f83b06b2310869bcf432d1136f532097017
dannybellieveit <131538258+dannybellieveit@users.noreply.github.com>
2025-10-22 16:43:04 +0100

    Update pinned status and order for images

--- public/images-data.js
+++ public/images-data.js
@@ -1,8 +1,8 @@
 // Lower pinnedOrder numbers appear first; omit to keep original data order amongst pinned items.
 window.imagesData = [
   { src: 'assets/photo1.jpg', title: 'Verbathim (Album)', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/2lmT9NiqohWoRf9yAxt4Ru?si=7291e441baaa452a', pinned: true, pinnedOrder: 1 },
-  { src: 'assets/photo2.jpg', title: 'TV Show', artist: 'Katie Gregson-MacLeod', spotifyUrl: 'https://open.spotify.com/track/0hQZyBWcYejAzb9WYM96pr?si=866aa6756cb24293', pinned: true, pinnedOrder: 2 },
-  { src: 'assets/photo3.jpg', title: 'We Need To Talk', artist: 'Matt Maltese', spotifyUrl: 'https://open.spotify.com/track/1gqMcDslzFtgOsAZDY58JX?si=f1e735a57f8e4076', pinned: true, pinnedOrder: 3 },
+  { src: 'assets/photo2.jpg', title: 'TV Show', artist: 'Katie Gregson-MacLeod', spotifyUrl: 'https://open.spotify.com/track/0hQZyBWcYejAzb9WYM96pr?si=866aa6756cb24293', pinned: false },
+  { src: 'assets/photo3.jpg', title: 'We Need To Talk', artist: 'Matt Maltese', spotifyUrl: 'https://open.spotify.com/track/1gqMcDslzFtgOsAZDY58JX?si=f1e735a57f8e4076', pinned: false },
   { src: 'assets/photo4.jpg', title: 'Alone At The Party', artist: 'Sam Tompkins', spotifyUrl: 'https://open.spotify.com/track/0YO7moiEboCUBDFf0hefSk?si=6abe803a16734b2e', pinned: false },
   { src: 'assets/photo5.jpg', title: 'Dollar Signs', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/6YyEuMnthCvLOZCGEbksAF?si=ea15d86f0f7d416d', pinned: false },
   { src: 'assets/photo6.jpg', title: 'Coming Closer', artist: 'Duckwrth', spotifyUrl: 'https://open.spotify.com/track/238p3EKRYESqsZdgE5DCDR?si=7f4332128f954948', pinned: false },
@@ -13,15 +13,15 @@ window.imagesData = [
   { src: 'assets/photo11.jpg', title: '54321', artist: 'April', spotifyUrl: 'https://open.spotify.com/track/6Vn5hk8NQIkzGsdkx5nF4q?si=42dbda986fb842fe', pinned: false },
   { src: 'assets/photo12.jpg', title: 'Boy Clothes', artist: 'Nxdia', spotifyUrl: 'https://open.spotify.com/track/7nuCxvFvVT5YEAjSDd6Glr?si=a241207fac3348d8', pinned: false },
   { src: 'assets/photo13.jpg', title: 'Options (feat. Lil Baby)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/0aQD4RI0U4pHWzNzQWTq9r?si=e5080b16576548f3', pinned: false },
-  { src: 'assets/photo14.jpg', title: '305 (feat. Bryson Tiller)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/494f07w2ArJNlkwnWWZViK?si=a3ff4f4b26ce4a53', pinned: false },
+  { src: 'assets/photo14.jpg', title: '305 (feat. Bryson Tiller)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/494f07w2ArJNlkwnWWZViK?si=a3ff4f4b26ce4a53', pinned: true, pinnedOrder: 2},
   { src: 'assets/photo15.jpg', title: 'Stick Of Gum', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/7DvOMvKBZESff6Etf0v9MY?si=c31005880f374552', pinned: false },
   { src: 'assets/photo16.jpg', title: 'congrats! u did it!', artist: 'flowerovlove', spotifyUrl: 'https://open.spotify.com/track/5bp8CvtJ8pbh2OK2HSfTwE?si=6895384a7d2343b5', pinned: false },
-  { src: 'assets/photo17.jpg', title: 'Appetite', artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/0q5zMDVszIVZb7kPi2XiOj?si=34033487b3344e8a', pinned: false },
+  //{ src: 'assets/photo17.jpg', title: 'Appetite', artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/0q5zMDVszIVZb7kPi2XiOj?si=34033487b3344e8a', pinned: false },
   { src: 'assets/photo18.jpg', title: 'Body On Me', artist: 'Nxdia', spotifyUrl: 'https://open.spotify.com/track/6MglZeuPDQwHzZTxrRZfCW?si=11e2487a15304c89', pinned: false },
-  { src: 'assets/photo19.jpg', title: 'Man In The Middle', artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/0JEsYciJ7ESWOckO7cSf4P?si=c8ee4129ed56416c', pinned: false },
+  //{ src: 'assets/photo19.jpg', title: 'Man In The Middle', artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/0JEsYciJ7ESWOckO7cSf4P?si=c8ee4129ed56416c', pinned: false },
   { src: 'assets/photo20.jpg', title: 'No Cameo', artist: 'Babymorocco', spotifyUrl: 'https://open.spotify.com/track/4H4w1qDifvKDRf9JIds8o9?si=dd09fee26fec450d', pinned: false },
   { src: 'assets/photo21.jpg', title: 'Drama', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/40ROAtBj7soL5rbqXQjU4k?si=2add6b9543114f4f', pinned: false },
-  { src: 'assets/photo22.jpg', title: "She's So Handsome", artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/1G3Nm7gbxkjOtFmZDoMEc4?si=0173dfb87b2c4bbb', pinned: false },
+  //{ src: 'assets/photo22.jpg', title: "She's So Handsome", artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/1G3Nm7gbxkjOtFmZDoMEc4?si=0173dfb87b2c4bbb', pinned: false },
   { src: 'assets/photo23.jpg', title: "Team", artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/582bQD4BTrPGaV9yrMWrUq?si=1e2468fb61e943fb', pinned: false },
   // { src: 'assets/photo24.jpg', title: "Magnolias", artist: 'ROSALÍA', spotifyUrl: 'https://www.rosalia.com' }
 
