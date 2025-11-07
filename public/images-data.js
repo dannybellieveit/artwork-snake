75489e9dbffab8ace275de62dee738771e6f0571
dannybellieveit <131538258+dannybellieveit@users.noreply.github.com>
2025-11-07 00:10:51 +0000

    Update images-data.js

--- public/images-data.js
+++ public/images-data.js
@@ -1,6 +1,6 @@
 // Lower pinnedOrder numbers appear first; omit to keep original data order amongst pinned items.
 window.imagesData = [
-  { src: 'assets/photo1.jpg', title: 'Verbathim (Album)', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/2lmT9NiqohWoRf9yAxt4Ru?si=7291e441baaa452a', pinned: true, pinnedOrder: 1 },
+  { src: 'assets/photo1.jpg', title: 'Verbathim (Album)', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/2lmT9NiqohWoRf9yAxt4Ru?si=7291e441baaa452a', pinned: true, pinnedOrder: 2 },
   { src: 'assets/photo2.jpg', title: 'TV Show', artist: 'Katie Gregson-MacLeod', spotifyUrl: 'https://open.spotify.com/track/0hQZyBWcYejAzb9WYM96pr?si=866aa6756cb24293', pinned: false },
   { src: 'assets/photo3.jpg', title: 'We Need To Talk', artist: 'Matt Maltese', spotifyUrl: 'https://open.spotify.com/track/1gqMcDslzFtgOsAZDY58JX?si=f1e735a57f8e4076', pinned: false },
   { src: 'assets/photo4.jpg', title: 'Alone At The Party', artist: 'Sam Tompkins', spotifyUrl: 'https://open.spotify.com/track/0YO7moiEboCUBDFf0hefSk?si=6abe803a16734b2e', pinned: false },
@@ -13,7 +13,7 @@ window.imagesData = [
   { src: 'assets/photo11.jpg', title: '54321', artist: 'April', spotifyUrl: 'https://open.spotify.com/track/6Vn5hk8NQIkzGsdkx5nF4q?si=42dbda986fb842fe', pinned: false },
   { src: 'assets/photo12.jpg', title: 'Boy Clothes', artist: 'Nxdia', spotifyUrl: 'https://open.spotify.com/track/7nuCxvFvVT5YEAjSDd6Glr?si=a241207fac3348d8', pinned: false },
   { src: 'assets/photo13.jpg', title: 'Options (feat. Lil Baby)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/0aQD4RI0U4pHWzNzQWTq9r?si=e5080b16576548f3', pinned: false },
-  { src: 'assets/photo14.jpg', title: '305 (feat. Bryson Tiller)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/494f07w2ArJNlkwnWWZViK?si=a3ff4f4b26ce4a53', pinned: true, pinnedOrder: 2},
+  { src: 'assets/photo14.jpg', title: '305 (feat. Bryson Tiller)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/494f07w2ArJNlkwnWWZViK?si=a3ff4f4b26ce4a53', pinned: true, pinnedOrder: 3},
   { src: 'assets/photo15.jpg', title: 'Stick Of Gum', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/7DvOMvKBZESff6Etf0v9MY?si=c31005880f374552', pinned: false },
   { src: 'assets/photo16.jpg', title: 'congrats! u did it!', artist: 'flowerovlove', spotifyUrl: 'https://open.spotify.com/track/5bp8CvtJ8pbh2OK2HSfTwE?si=6895384a7d2343b5', pinned: false },
   //{ src: 'assets/photo17.jpg', title: 'Appetite', artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/0q5zMDVszIVZb7kPi2XiOj?si=34033487b3344e8a', pinned: false },
@@ -23,7 +23,7 @@ window.imagesData = [
   { src: 'assets/photo21.jpg', title: 'Drama', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/40ROAtBj7soL5rbqXQjU4k?si=2add6b9543114f4f', pinned: false },
   //{ src: 'assets/photo22.jpg', title: "She's So Handsome", artist: 'Arthur Hill', spotifyUrl: 'https://open.spotify.com/track/1G3Nm7gbxkjOtFmZDoMEc4?si=0173dfb87b2c4bbb', pinned: false },
   { src: 'assets/photo23.jpg', title: "Team", artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/582bQD4BTrPGaV9yrMWrUq?si=1e2468fb61e943fb', pinned: false },
-  { src: 'assets/photo24.jpg', title: "Magnolias", artist: 'ROSALÍA', spotifyUrl: 'https://open.spotify.com/track/7GF92haKwJg9WUdnkTxRtr', pinned: true },
+  { src: 'assets/photo24.jpg', title: "Magnolias", artist: 'ROSALÍA', spotifyUrl: 'https://open.spotify.com/track/7GF92haKwJg9WUdnkTxRtr', pinned: true pinnedOrder: 1},
   { src: 'assets/photo25.jpg', title: "Missed Again (EP)", artist: 'Arthur Hill', spotifyUrl: 'https://www.instagram.com/p/DPUJim3CYVL/', pinned: false },
   { src: 'assets/photo26.jpg', title: "HONESTLY, OBVIOUSLY", artist: 'Joseph Lawrence', spotifyUrl: 'https://open.spotify.com/prerelease/5IZ3IcKQawSbLsOECRj7xo', pinned: false },
   
