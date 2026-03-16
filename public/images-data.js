fd6153c304bdd1b33491901b326721fd8285c224
dannybellieveit <131538258+dannybellieveit@users.noreply.github.com>
2026-03-16 16:33:39 +0000

    Add new image entry for 'try praying' by Orlando

--- public/images-data.js
+++ public/images-data.js
@@ -28,6 +28,8 @@ window.imagesData = [
   { src: 'assets/photo26.jpg', title: "HONESTLY, OBVIOUSLY", artist: 'Joseph Lawrence', spotifyUrl: 'https://open.spotify.com/prerelease/5IZ3IcKQawSbLsOECRj7xo', pinned: false },
   { src: 'assets/photo27.jpg', title: "If My Heart Was A House", artist: 'JADE', spotifyUrl: 'https://open.spotify.com/track/1I5XmBxIpd9my9UFmjjF3g', pinned: true, pinnedOrder: 3},
   { src: 'assets/photo28.jpg', title: "Climbing Up", artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/5RstLcurfXb5TSQnLi7kYf', pinned: false },
+  { src: 'assets/photo29.jpg', title: "try praying", artist: 'Orlando', spotifyUrl: 'https://open.spotify.com/track/5mQMxc82vZuy9goTAEl4JJ', pinned: false },
+
   
 
 
