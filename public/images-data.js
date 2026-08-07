2917c67fd9d0f1e7207dc2f34f0004415d6be467
dannybellieveit <131538258+dannybellieveit@users.noreply.github.com>
2026-08-07 16:58:16 +0100

    Comment out image entries in images-data.js
    Commented out several image entries in imagesData.

--- public/images-data.js
+++ public/images-data.js
@@ -1,16 +1,16 @@
 // Lower pinnedOrder numbers appear first; omit to keep original data order amongst pinned items.
 window.imagesData = [
   { src: 'assets/photo1.jpg', title: 'Verbathim (Album)', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/2lmT9NiqohWoRf9yAxt4Ru', pinned: true, pinnedOrder: 2 },
-  { src: 'assets/photo2.jpg', title: 'TV Show', artist: 'Katie Gregson-MacLeod', spotifyUrl: 'https://open.spotify.com/track/0hQZyBWcYejAzb9WYM96pr', pinned: false },
+  //{ src: 'assets/photo2.jpg', title: 'TV Show', artist: 'Katie Gregson-MacLeod', spotifyUrl: 'https://open.spotify.com/track/0hQZyBWcYejAzb9WYM96pr', pinned: false },
   { src: 'assets/photo3.jpg', title: 'We Need To Talk', artist: 'Matt Maltese', spotifyUrl: 'https://open.spotify.com/track/1gqMcDslzFtgOsAZDY58JX', pinned: false },
   { src: 'assets/photo4.jpg', title: 'Alone At The Party', artist: 'Sam Tompkins', spotifyUrl: 'https://open.spotify.com/track/0YO7moiEboCUBDFf0hefSk', pinned: false },
   { src: 'assets/photo5.jpg', title: 'Dollar Signs', artist: 'Nemahsis', spotifyUrl: 'https://open.spotify.com/track/6YyEuMnthCvLOZCGEbksAF', pinned: false },
   { src: 'assets/photo6.jpg', title: 'Coming Closer', artist: 'Duckwrth', spotifyUrl: 'https://open.spotify.com/track/238p3EKRYESqsZdgE5DCDR', pinned: false },
   { src: 'assets/photo7.jpg', title: 'This Is The Place', artist: 'Tom Grennan', spotifyUrl: 'https://open.spotify.com/track/0UtoTf0kuz8x6Zfy59r8hp', pinned: false },
   { src: 'assets/photo8.jpg', title: 'Skin', artist: 'Joy Crookes', spotifyUrl: 'https://open.spotify.com/track/7k8b5u5fisGTDNahrJK6dw', pinned: false },
-  { src: 'assets/photo9.jpg', title: 'Paradise', artist: 'Griff', spotifyUrl: 'https://open.spotify.com/track/7nfaD0trhQiStQ8DOQRC0h', pinned: false },
-  { src: 'assets/photo10.jpg', title: 'LMLY', artist: 'Jackson Wang', spotifyUrl: 'https://open.spotify.com/track/3kPoV6L9vXpbxoM4Ux0KnX', pinned: false },
-  { src: 'assets/photo11.jpg', title: '54321', artist: 'April', spotifyUrl: 'https://open.spotify.com/track/6Vn5hk8NQIkzGsdkx5nF4q', pinned: false },
+  //{ src: 'assets/photo9.jpg', title: 'Paradise', artist: 'Griff', spotifyUrl: 'https://open.spotify.com/track/7nfaD0trhQiStQ8DOQRC0h', pinned: false },
+  //{ src: 'assets/photo10.jpg', title: 'LMLY', artist: 'Jackson Wang', spotifyUrl: 'https://open.spotify.com/track/3kPoV6L9vXpbxoM4Ux0KnX', pinned: false },
+  //{ src: 'assets/photo11.jpg', title: '54321', artist: 'April', spotifyUrl: 'https://open.spotify.com/track/6Vn5hk8NQIkzGsdkx5nF4q', pinned: false },
   { src: 'assets/photo12.jpg', title: 'Boy Clothes', artist: 'Nxdia', spotifyUrl: 'https://open.spotify.com/track/7nuCxvFvVT5YEAjSDd6Glr', pinned: false },
   { src: 'assets/photo13.jpg', title: 'Options (feat. Lil Baby)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/0aQD4RI0U4pHWzNzQWTq9r', pinned: false },
   { src: 'assets/photo14.jpg', title: '305 (feat. Bryson Tiller)', artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/494f07w2ArJNlkwnWWZViK', pinned: true, pinnedOrder: 4},
@@ -28,9 +28,10 @@ window.imagesData = [
   { src: 'assets/photo26.jpg', title: "HONESTLY, OBVIOUSLY", artist: 'Joseph Lawrence', spotifyUrl: 'https://open.spotify.com/prerelease/5IZ3IcKQawSbLsOECRj7xo', pinned: false },
   { src: 'assets/photo27.jpg', title: "If My Heart Was A House", artist: 'JADE', spotifyUrl: 'https://open.spotify.com/track/1I5XmBxIpd9my9UFmjjF3g', pinned: true, pinnedOrder: 3},
   { src: 'assets/photo28.jpg', title: "Climbing Up", artist: 'Jordan Adetunji', spotifyUrl: 'https://open.spotify.com/track/5RstLcurfXb5TSQnLi7kYf', pinned: false },
-  { src: 'assets/photo29.jpg', title: "try praying", artist: 'Orlando', spotifyUrl: 'https://open.spotify.com/track/5mQMxc82vZuy9goTAEl4JJ', pinned: false },
+  //{ src: 'assets/photo29.jpg', title: "try praying", artist: 'Orlando', spotifyUrl: 'https://open.spotify.com/track/5mQMxc82vZuy9goTAEl4JJ', pinned: false },
   { src: 'assets/photo30.jpg', title: "Lovemesick, Anchor & Fuck Me Up", artist: 'Nxdia', spotifyUrl: 'https://open.spotify.com/album/7zpQ3LP0JDe5mLEl10MPiv', pinned: false },
   { src: 'assets/photo31.jpg', title: "Hell For Leather", artist: 'Orlando', spotifyUrl: 'https://open.spotify.com/track/3emgzRa8ojyoB90K2QM57Y', pinned: false },
+  { src: 'assets/photo32.jpg', title: "Fun", artist: 'The Big Moon', spotifyUrl: 'https://open.spotify.com/track/36u7bz30t8wlYlyKhN7LKG', pinned: false },
 
 
   
